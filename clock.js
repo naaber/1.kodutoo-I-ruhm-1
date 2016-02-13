3abdbd241ac080d11f5c6f5294eee8afa80fa6bc
naaber <martnaa@greeny.cs.tlu.ee>
2016-02-13 12:41:20 +0200

    Singelton pattern transformed into

--- clock.js
+++ clock.js
@@ -1,155 +1,171 @@
-window.onload = function(){
+(function(){
+  "use strict";
 
-  var clock = document.getElementById("clock");
-  var clock_year = document.getElementById("clock_year");
-  var clock_month = document.getElementById("clock_month");
-  var clock_date = document.getElementById("clock_date");
-  var clock_day = document.getElementById("clock_day");
-  var clock_hour = document.getElementById("clock_hour");
-  var clock_minute = document.getElementById("clock_minute");
-  var clock_second = document.getElementById("clock_second");
+  var Kell = function(){
 
-  var theme = "style_1";
-  var day_as_word = "style_1";
-
-  writeTime();
-
-  window.setInterval(function(){writeTime();}, 1000);
-
-  /* Kasutaja saab kahekordse hiireklõpsuga muuta tausta värvi, hiireklõpsud
-  peavad toimuma kella peal. */
-  clock.addEventListener('dblclick', function(){
-    if(theme === "style_1"){
-      document.body.style.backgroundColor="#989898";
-      document.body.style.color="#FFFFFF";
-      theme = "style_2";
-    } else {
-      document.body.style.backgroundColor="#FFFFFF";
-      document.body.style.color="#989898";
-      theme = "style_1";
+    if(Kell.instance){
+      return Kell.instance;
     }
-  });
 
-  /* Kui kasutaja vajutab hiirega kuupäeva peal, siis tuleb
-   esile nädalapäeva nimetus. */
-  clock_date.addEventListener('click', function(){
+    Kell.instance = this;
 
-    var today = new Date();
-    var day = today.getDay();
-    var days = ["ESMASPÄEV", "TEISIPÄEV", "KOLMAPÄEV", "NELJAPÄEV", "REEDE", "LAUPÄEV", "PÜHAPÄEV"];
+    this.clock = document.querySelector(".outer_clock");
+    this.clock_year = document.querySelector("#clock_year");
+    this.clock_month = document.querySelector("#clock_month");
+    this.clock_date = document.querySelector("#clock_date");
+    this.clock_day = document.querySelector("#clock_day");
+    this.clock_hour = document.querySelector("#clock_hour");
+    this.clock_minute = document.querySelector("#clock_minute");
+    this.clock_second = document.querySelector("#clock_second");
 
-    if(day_as_word === "style_1"){
-      clock_day.innerHTML = days[day - 1];
-      day_as_word = "style_2";
-    } else {
-      clock_day.innerHTML = "";
-      day_as_word = "style_1";
+    this.today = new Date();
+    this.year = this.today.getFullYear();
+    this.month = this.today.getMonth() + 1;
+    this.date = this.today.getDate();
+    this.day = this.today.getDay();
+    this.hour = this.today.getHours();
+    this.minute = this.today.getMinutes();
+    this.second = this.today.getSeconds();
+
+    this.theme = "style_1";
+    this.day_as_word = "style_1";
+    this.days = ["ESMASPÄEV", "TEISIPÄEV", "KOLMAPÄEV", "NELJAPÄEV", "REEDE", "LAUPÄEV", "PÜHAPÄEV"];
+
+    this.init();
+  };
+
+  window.Kell = Kell;
+
+  Kell.prototype = {
+
+    init: function(){
+
+      this.writeTime();
+      this.changeBackground();
+      this.writeDay();
+      this.changeSize();
+    },
+    writeTime: function(){
+
+      clock_year.innerHTML = "";
+      clock_month.innerHTML = "";
+      clock_date.innerHTML = "";
+      clock_hour.innerHTML = "";
+      clock_minute.innerHTML = "";
+      clock_second.innerHTML = "";
+
+      for(var i = 1; i < 13; i++){
+        if(i%2 === 0){
+          clock_month.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_month") + "<br>";
+        }else{
+          clock_month.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_month");
+        }
+      }
+
+      for(i = 1; i < 37; i++){
+        if(i > 31){
+          clock_date.innerHTML += "00";
+        }else if(i%6 === 0){
+          clock_date.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_date") + "<br>";
+        }else{
+          clock_date.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_date");
+        }
+      }
+
+      for(i = 1; i < 25; i++){
+        if(i === 24){
+          clock_year.innerHTML += this.showCurrentTime(2024, "select_year");
+          clock_hour.innerHTML += this.showCurrentTime(this.setZeroBefore(0), "select_hour");
+        }else if(i%4 === 0){
+          clock_year.innerHTML += this.showCurrentTime(i + 2000, "select_year") + "<br>";
+          clock_hour.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_hour") + "<br>";
+        }else{
+          clock_year.innerHTML += this.showCurrentTime(i + 2000, "select_year");
+          clock_hour.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_hour");
+        }
+      }
+
+      for(i = 1; i < 61; i++){
+        if(i === 60){
+          clock_minute.innerHTML += this.showCurrentTime(this.setZeroBefore(0), "select_minute");
+          clock_second.innerHTML += this.showCurrentTime(this.setZeroBefore(0), "select_second");
+        }else if(i%10 === 0){
+          clock_minute.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_minute") + "<br>";
+          clock_second.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_second") + "<br>";
+        }else{
+          clock_minute.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_minute");
+          clock_second.innerHTML += this.showCurrentTime(this.setZeroBefore(i), "select_second");
+        }
+      }
+    },
+    showCurrentTime: function(number, format){
+
+      if(format === "select_year" && number === this.year){
+          number = "<font color='black'><b>" + number + "</font></b>";
+      }else if(format === "select_month" && number === this.setZeroBefore(this.month)){
+          number = "<font color='black'><b>" + number + "</font></b>";
+      }else if(format === "select_date" && number === this.setZeroBefore(this.date)){
+          number = "<font color='black'><b>" + number + "</font></b>";
+      }else if(format === "select_hour" && number === this.setZeroBefore(this.hour)){
+          number = "<font color='black'><b>" + number + "</font></b>";
+      }else if(format === "select_minute" && number === this.setZeroBefore(this.minute)){
+          number = "<font color='black'><b>" + number + "</font></b>";
+      }else if(format === "select_second" && number === this.setZeroBefore(this.second)){
+          number = "<font color='black'><b>" + number + "</font></b>";
+      }
+      return number;
+    },
+    setZeroBefore: function(number){
+      if(number < 10){
+        number = '0' + number;
+      }
+      return number;
+    },
+    changeBackground: function(){
+      clock.addEventListener('dblclick', this.changeColor.bind(this));
+    },
+    changeColor: function(){
+      if(this.theme === "style_1"){
+        document.querySelector("body").style.backgroundColor="#989898";
+        document.querySelector("body").style.color="#FFFFFF";
+        this.theme = "style_2";
+      } else {
+        document.querySelector("body").style.backgroundColor="#FFFFFF";
+        document.querySelector("body").style.color="#989898";
+        this.theme = "style_1";
+      }
+    },
+    writeDay: function(){
+      clock_date.addEventListener('click', this.selectDay.bind(this));
+    },
+    selectDay: function(){
+      if(this.day_as_word === "style_1"){
+        clock_day.innerHTML = this.days[this.day - 1];
+        this.day_as_word = "style_2";
+      } else {
+        clock_day.innerHTML = "";
+        this.day_as_word = "style_1";
+      }
+    },
+    changeSize: function(){
+      window.addEventListener('keypress', this.smallBigSize.bind(this));
+    },
+    smallBigSize: function(){
+      if(event.keyCode === 49) {
+        document.querySelector("body").style.fontSize="10px";
+      }else if(event.keyCode === 50){
+        document.querySelector("body").style.fontSize="15px";
+      }else if(event.keyCode === 51){
+        document.querySelector("body").style.fontSize="20px";
+      }
     }
-  });
+  };
 
-  /* Numbritega 1,2,3 saab kasutaja muuta kella suurust. */
-  window.addEventListener('keypress', function(event){
+  window.onload = function(){
+    var app = new Kell();
+  };
 
-    var key_code = event.keyCode;
 
-    if(key_code === 49) {
-      document.body.style.fontSize="10px";
-    }else if(key_code === 50){
-      document.body.style.fontSize="15px";
-    }else if(key_code === 51){
-      document.body.style.fontSize="20px";
-    }
-  });
-};
+})();
 
-/* Siin tuuakse tsükleid kasutades esile kõik numbrid, mis kella ja kuupäeva
-jaoks vajalikud on. */
-function writeTime(){
-
-  clock_year.innerHTML = "";
-  clock_month.innerHTML = "";
-  clock_date.innerHTML = "";
-  clock_hour.innerHTML = "";
-  clock_minute.innerHTML = "";
-  clock_second.innerHTML = "";
-
-  for(i = 1; i < 13; i++){
-    if(i%2 === 0){
-      clock_month.innerHTML += showCurrentTime(setZeroBefore(i), "select_month") + "<br>";
-    }else{
-      clock_month.innerHTML += showCurrentTime(setZeroBefore(i), "select_month");
-    }
-  }
-
-  for(i = 1; i < 37; i++){
-    if(i > 31){
-      clock_date.innerHTML += "00";
-    }else if(i%6 === 0){
-      clock_date.innerHTML += showCurrentTime(setZeroBefore(i), "select_date") + "<br>";
-    }else{
-      clock_date.innerHTML += showCurrentTime(setZeroBefore(i), "select_date");
-    }
-  }
-
-  for(i = 1; i < 25; i++){
-    if(i === 24){
-      clock_year.innerHTML += showCurrentTime(2024, "select_year");
-      clock_hour.innerHTML += showCurrentTime(setZeroBefore(0), "select_hour");
-    }else if(i%4 === 0){
-      clock_year.innerHTML += showCurrentTime(i + 2000, "select_year") + "<br>";
-      clock_hour.innerHTML += showCurrentTime(setZeroBefore(i), "select_hour") + "<br>";
-    }else{
-      clock_year.innerHTML += showCurrentTime(i + 2000, "select_year");
-      clock_hour.innerHTML += showCurrentTime(setZeroBefore(i), "select_hour");
-    }
-  }
-
-  for(i = 1; i < 61; i++){
-    if(i === 60){
-      clock_minute.innerHTML += showCurrentTime(setZeroBefore(0), "select_minute");
-      clock_second.innerHTML += showCurrentTime(setZeroBefore(0), "select_second");
-    }else if(i%10 === 0){
-      clock_minute.innerHTML += showCurrentTime(setZeroBefore(i), "select_minute") + "<br>";
-      clock_second.innerHTML += showCurrentTime(setZeroBefore(i), "select_second") + "<br>";
-    }else{
-      clock_minute.innerHTML += showCurrentTime(setZeroBefore(i), "select_minute");
-      clock_second.innerHTML += showCurrentTime(setZeroBefore(i), "select_second");
-    }
-  }
-}
-
-function setZeroBefore(number){
-
-  if(number < 10){
-    number = '0' + number;
-  }
-  return number;
-}
-
-/* Siin märgistatakse rasvalt ära praegune kellaaeg. */
-function showCurrentTime(number, format){
-
-  var today = new Date();
-
-  var year = today.getFullYear();
-  var month = today.getMonth() + 1;
-  var date = today.getDate();
-  var hour = today.getHours();
-  var minute = today.getMinutes();
-  var second = today.getSeconds();
-
-  if(format === "select_year" && number === year){
-      number = "<font color='black'><b>" + number + "</font></b>";
-  }else if(format === "select_month" && number === setZeroBefore(month)){
-      number = "<font color='black'><b>" + number + "</font></b>";
-  }else if(format === "select_date" && number === setZeroBefore(date)){
-      number = "<font color='black'><b>" + number + "</font></b>";
-  }else if(format === "select_hour" && number === setZeroBefore(hour)){
-      number = "<font color='black'><b>" + number + "</font></b>";
-  }else if(format === "select_minute" && number === setZeroBefore(minute)){
-      number = "<font color='black'><b>" + number + "</font></b>";
-  }else if(format === "select_second" && number === setZeroBefore(second)){
-      number = "<font color='black'><b>" + number + "</font></b>";
-  }
-  return number;
-}
+//window.setInterval(function(){writeTime();}, 1000);
